1abfda9679288c0a3e805d6be465d8851a9c5cee
Y1080p <1919286811@qq.com>
2025-12-26 16:14:04 +0800

    Change API_BASE_URL to use VITE_API_BASE_URL
    Updated API base URL to use environment variable.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = '/api'
+// 替换第3行的代码
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + '/api'
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
